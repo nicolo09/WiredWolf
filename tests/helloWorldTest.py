6ee3eb7ef194ed46c855301332877ff027cae06b
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2025-07-31 12:00:43 +0200

    Initialized project

---
+++ tests/helloWorldTest.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestExample(unittest.TestCase):
+    
+    def setUp(self):
+        # Before each test
+        pass
+    
+    def tearDown(self):
+        # After each test
+        pass
+    
+    def test_example(self):
+        self.assertTrue(True)
