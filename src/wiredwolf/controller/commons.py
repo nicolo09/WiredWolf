e3143e86384a99d13e91adfec39e9cfc5166867f
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2025-09-01 16:39:07 +0200

    Added UUID to Peer

--- src/wiredwolf/controller/commons.py
+++ src/wiredwolf/controller/commons.py
@@ -1,12 +1,21 @@
+import uuid
+
+
 class Peer:
     """Represents a peer in the network."""
 
     _name: str
     _address: str
+    _uuid: uuid.UUID
 
     def __init__(self, name: str, address: str):
         self._name = name
         self._address = address
+        self._uuid = uuid.uuid4()
+
+    @property
+    def uuid(self):
+        return self._uuid
 
     @property
     def address(self):
