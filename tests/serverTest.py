99cca3ce82d75d6a84d3f74a500f33b00e1f4258
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2025-09-29 22:22:33 +0200

    Implemented server connection test

--- tests/serverTest.py
+++ tests/serverTest.py
@@ -3,7 +3,7 @@ import unittest
 
 from wiredwolf.controller.commons import PasswordRequest, Peer
 from wiredwolf.controller.connections import MessageHandlerFactory
-from wiredwolf.controller.lobbies import Lobby
+from wiredwolf.controller.lobbies import Lobby, LobbyBrowser
 from wiredwolf.controller.server import GameServer
 
 
@@ -21,4 +21,15 @@ class ServerTest(unittest.TestCase):
         handler.send_obj(client_sock, pass_req)
         self.assertEqual(handler.receive_obj(client_sock), lobby)
         client_sock.close()
+        server.stop_new_connections()
+        
+    def test_client_connect_to_server(self):
+        PASSWORD = "password123"
+        lobby: Lobby = Lobby("Test Lobby", PASSWORD)
+        lobbyBrowser = LobbyBrowser()
+        server: GameServer = GameServer(lobby)
+        mySelf = Peer("test_user", "127.0.0.1")
+        sock, recvLobby = lobbyBrowser.connect_to_lobby_directly(mySelf, ("127.0.0.1", server.connection_socket.getsockname()[1]), PASSWORD)
+        self.assertEqual(recvLobby, lobby)
+        sock.close()
         server.stop_new_connections()
