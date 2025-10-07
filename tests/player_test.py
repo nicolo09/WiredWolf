c6be76ea53d3425d4ad86873e16729f7ebeac4d4
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2025-10-07 22:15:31 +0200

    Added test case for "create_players" method

--- tests/player_test.py
+++ tests/player_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from wiredwolf.model.player import Player, Role, Status
+from wiredwolf.model.player import *
 
 class TestPlayer(unittest.TestCase):
 
@@ -8,6 +8,37 @@ class TestPlayer(unittest.TestCase):
         self.villager = Player("Alice", Role.VILLAGER)
         self.werewolf = Player("Bob", Role.WEREWOLF)
         
+    def test_player_creation(self):
+        ids: list[str] = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Hank"]
+        special_roles: list[Role] = [Role.ESCORT, Role.CLAIRVOYANT]
+        players = create_players(ids, special_roles)
+        self.assertEqual(len(players), 8)
+
+        villagers_count = 0
+        werewolves_count = 0
+        escorts_count = 0
+        clairvoyants_count = 0
+        mediums_count = 0
+            
+        for player in players:
+            match player.role:
+                case Role.VILLAGER:
+                    villagers_count += 1
+                case Role.WEREWOLF:
+                    werewolves_count += 1
+                case Role.ESCORT:
+                    escorts_count += 1
+                case Role.CLAIRVOYANT:
+                    clairvoyants_count += 1
+                case Role.MEDIUM:
+                    mediums_count += 1
+        
+        self.assertEqual(escorts_count, 1)
+        self.assertEqual(clairvoyants_count, 1)
+        self.assertEqual(werewolves_count, 2)
+        self.assertEqual(villagers_count, 4)
+        self.assertEqual(mediums_count, 0)
+
     def test_initial_status(self):
         self.assertEqual(self.villager.status, Status.ALIVE)
         self.assertEqual(self.werewolf.status, Status.ALIVE)
