329f15e38a9f3549aa61d8f59d4cf88bdeaf4bc9
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2025-10-26 11:03:00 +0100

    Changed plugin_test from unittest to pytest

--- tests/controller/plugins_test.py
+++ tests/controller/plugins_test.py
@@ -1,6 +1,7 @@
 import threading
 from time import sleep
-import unittest
+
+import pytest
 
 from tests.controller.utils import TestFactory
 from wiredwolf.controller.commons import Peer
@@ -10,43 +11,36 @@ from wiredwolf.controller.messages import BaseMessage, ChatMessage
 from wiredwolf.controller.server import GameServer
 from wiredwolf.controller.server_plugins import ChatPlugin
 
-
-class PluginTest(unittest.TestCase):
-    """This class tests plugins attached to a GameServer.
-    To obtain a GameServer and its connected clients it uses a TestFactory which by default gives a TCP server and clients.
-    If the connection protocol is changed the factory and this test should be adapted in order to run this tests with all protocols available.
-    """
-
+@pytest.fixture
+def gameServerAndClients():
     gameServer: GameServer
     clients: list[ClientConnectionHandler]
+    gameServer, clients = TestFactory.create_tcp_server_with_connected_clients(2, Lobby("Test Lobby"))
+    yield gameServer, clients
+    gameServer.close()
+    for client in clients:
+        client.close()
 
-    def setUp(self) -> None:
-        self.gameServer, self.clients = TestFactory.create_tcp_server_with_connected_clients(2, Lobby("Test Lobby"))
 
-    def tearDown(self) -> None:
-        super().tearDown()
-        self.gameServer.close()
-        for client in self.clients:
-            client.close()
+def test_chat_plugin(gameServerAndClients: tuple[GameServer, list[ClientConnectionHandler]]): #TODO: Flaky test, sometimes message is not received or wrong message type
+    gameServer, clients = gameServerAndClients
+    MESSAGE: str = "Hello from client 0"
+    message_received: threading.Event = threading.Event()
 
-    def test_chat_plugin(self): #TODO: Flaky test, sometimes message is not received or wrong message type
-        MESSAGE: str = "Hello from client 0"
-        message_received: threading.Event = threading.Event()
+    def on_message(msg: BaseMessage):
+        if isinstance(msg, ChatMessage):
+            assert msg.message == MESSAGE
+            message_received.set()
 
-        def on_message(msg: BaseMessage):
-            if isinstance(msg, ChatMessage):
-                self.assertEqual(msg.message, MESSAGE)
-                message_received.set()
-
-        chat_plugin = ChatPlugin()
-        self.gameServer.add_plugin(chat_plugin)
-        self.clients[1].set_on_message(on_message)
-        peer: Peer = Peer("client_0")
-        self.clients[0].send_obj(
-            ChatMessage(sender=peer, message=MESSAGE)
-        )
-        waited = 0.0
-        while not message_received.is_set() and waited < 3:
-            sleep(0.1)
-            waited += 0.1
-        self.assertTrue(message_received.is_set(), "Chat message was not received by client 1")
+    chat_plugin = ChatPlugin()
+    gameServer.add_plugin(chat_plugin)
+    clients[1].set_on_message(on_message)
+    peer: Peer = Peer("client_0")
+    clients[0].send_obj(
+        ChatMessage(sender=peer, message=MESSAGE)
+    )
+    waited = 0.0
+    while not message_received.is_set() and waited < 3:
+        sleep(0.1)
+        waited += 0.1
+    assert message_received.is_set(), "Chat message was not received by client 1"
