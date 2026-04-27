1d5e27bbbf3479f75630b4594f033c61d76b5959
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2026-04-27 12:40:06 +0200

    Added method to go to home screen if error is fatal and game can't continue after it

--- src/wiredwolf/view/custom_events.py
+++ src/wiredwolf/view/custom_events.py
@@ -463,7 +463,12 @@ class EventSender(ABC):
     def error_ended_go_to_screen(self, next_screen:Screens)->None:
         """Sends the end of error to the view and changes screen to the given one"""
         raise NotImplementedError("Please implement this method")
-    
+
+    @abstractmethod
+    def error_ended_go_to_home(self)->None:
+        """Sends the end of error to the view and changes screen to the home screen. Controller needs to call view.reset to properly reset the view"""
+        raise NotImplementedError("Please implement this method")
+
     @abstractmethod
     def player_is_dead(self)->None:
         """Triggers ghost player view, called after player is executed or if player is killed by werewolves at night"""
@@ -653,6 +658,9 @@ class CustomEventSender(EventSender):
 
     def error_ended_go_to_screen(self, next_screen:Screens)->None:
         self.send_event_end_error(next_screen)
+    
+    def error_ended_go_to_home(self)->None:
+        self.send_event_end_error(Screens.HOME)
 
     def player_is_dead(self)->None:
         self.send_event_dead_player()
